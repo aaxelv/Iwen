46aa36f289f15135379ba1acb1788595eb0ef3ce
Axel Vincent <axel.vincent@student.ecp.fr>
2020-04-26 11:20:38 +0200

    first routes

---
+++ src/components/Photo.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Photo extends Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    render () {
+        return (
+            <div>Photo</div>
+        );
+    }
+}
+
+export default Photo;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import Home from './components/Home';
+import Photo from './components/Photo';
 
 import './style/main.less';
 
-class Welcome extends React.Component {
-    render () {
-        return <h1>Hello World from React boilerplate</h1>;
-    }
-}
-ReactDOM.render(<Welcome />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route
+                exact
+                path="/"
+                component={Home}
+            />
+            <Route path="/photo" component={Photo} />
+        </Switch>
+    </BrowserRouter>
+    ,
+    document.getElementById('root')
+);
